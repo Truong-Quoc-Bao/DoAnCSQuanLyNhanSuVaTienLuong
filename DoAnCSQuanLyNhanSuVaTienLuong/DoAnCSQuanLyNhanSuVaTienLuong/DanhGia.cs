bbf52b6854da0b8bd61b2ce0e8133732025a8bf5
thietam <tranthientam220404@gmail.com>
2024-11-17 01:26:19 +0700

    test2

--- DoAnCSQuanLyNhanSuVaTienLuong/DoAnCSQuanLyNhanSuVaTienLuong/DanhGia.cs
+++ DoAnCSQuanLyNhanSuVaTienLuong/DoAnCSQuanLyNhanSuVaTienLuong/DanhGia.cs
@@ -16,5 +16,10 @@ namespace DoAnCSQuanLyNhanSuVaTienLuong
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
