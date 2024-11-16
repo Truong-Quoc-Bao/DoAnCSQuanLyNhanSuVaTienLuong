e795753cbd3dc1d8d58ea43b7e621e1ebe9f7687
Trương Quốc Bảo <baotruong.190404@gmail.com>
2024-11-16 19:10:59 +0700

    cap nhat lan 2
    alo ae

--- DoAnCSQuanLyNhanSuVaTienLuong/DoAnCSQuanLyNhanSuVaTienLuong/Program.cs
+++ DoAnCSQuanLyNhanSuVaTienLuong/DoAnCSQuanLyNhanSuVaTienLuong/Program.cs
@@ -16,7 +16,7 @@ namespace DoAnCSQuanLyNhanSuVaTienLuong
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DangNhap());
         }
     }
 }
